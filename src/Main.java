79dea17ad7f61520231166a73793baa882c3dc89
henry8581 <henry8581@yahoo.com.tw>
2017-10-05 20:27:26 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int x,y;
+        for (x=1;x<=9;x++)
+
+        for (y=1;y<=9;y++)
+           System.out.println( x + "*" + y + "=" + x*y + "\t");
+        System.out.println();
+    }
+}
